7745a679a126f58f01149abb0d540e1daa8ea94f
Jeremy Kemp <jeremykemp@google.com>
2024-05-22 09:58:32 +0100

    Add Mali OpenCL ICD to PRODUCT_PACKAGES
    Bug: 324057328
    Test: clpeak
    Change-Id: I7b7de856d1bb090ace31d1938e69069e24e79d7f

--- device.mk
+++ device.mk
@@ -217,8 +217,17 @@ include device/google/gs-common/gpu/gpu.mk
 PRODUCT_PACKAGES += \
 	libGLES_mali \
 	vulkan.mali \
-	libOpenCL \
 	libgpudataproducer
+
+# Install the OpenCL ICD Loader
+PRODUCT_SOONG_NAMESPACES += external/OpenCL-ICD-Loader
+PRODUCT_PACKAGES += \
+       libOpenCL \
+       mali_icd__customer_pixel_opencl-icd_ARM.icd
+ifeq ($(DEVICE_IS_64BIT_ONLY),false)
+PRODUCT_PACKAGES += \
+	mali_icd__customer_pixel_opencl-icd_ARM32.icd
+endif
 
 PRODUCT_VENDOR_PROPERTIES += \
 	ro.hardware.vulkan=mali
